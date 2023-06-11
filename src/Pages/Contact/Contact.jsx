adc9ba9e351aa700e506f8f8f396ea20bd536198
Vishnu <r.vishnuvarthan48@gmail.com>
2023-06-11 19:35:29 +0530

    second

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
+import "./Contact.css"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='table'>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Chapman</td>
+            <td>@mchapman</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fatman</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Chapman</td>
+            <td>@mchapman</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fatman</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Chapman</td>
+            <td>@mchapman</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fatman</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
